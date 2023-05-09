37634e769d7a8f5c8a4c0122111ac1424273436b
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-05-09 09:40:05 -0600

    Maxwell Christian

---
+++ app/src/main/java/in/maxwellchristian/androiddemos/menu_demos/AllDemosActivity.java
@@ -0,0 +1,54 @@
+package in.maxwellchristian.androiddemos.menu_demos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import in.maxwellchristian.androiddemos.Login;
+import in.maxwellchristian.androiddemos.MessageDemo;
+import in.maxwellchristian.androiddemos.R;
+import in.maxwellchristian.androiddemos.TipCalculator;
+
+public class AllDemosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_demos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // as per the selected menu item navigate to the appropriate activity
+
+        Intent intent = new Intent();
+
+        if (item.getItemId() == R.id.omiToast) {
+            intent.setClass(AllDemosActivity.this, MessageDemo.class);
+        }
+
+        if (item.getItemId() == R.id.omiLogin) {
+            intent.setClass(AllDemosActivity.this, Login.class);
+        }
+
+        if (item.getItemId() == R.id.omiTipCalculator) {
+            intent.setClass(AllDemosActivity.this, TipCalculator.class);
+        }
+
+        AllDemosActivity.this.startActivity(intent);
+
+        return super.onOptionsItemSelected(item);
+    }
+}

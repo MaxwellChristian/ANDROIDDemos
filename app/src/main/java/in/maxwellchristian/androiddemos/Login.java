108005e6eeeaa772e7c28a07632bb4465d0564fd
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-05-10 10:11:00 -0600

    Maxwell Christian

--- app/src/main/java/in/maxwellchristian/androiddemos/Login.java
+++ app/src/main/java/in/maxwellchristian/androiddemos/Login.java
@@ -1,8 +1,12 @@
 package in.maxwellchristian.androiddemos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,7 +40,7 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(view -> {
 
             // check for the correct credentials
-            if( etUsername.getText().toString().equals("maxwell") && etPassword.getText().toString().equals("maxwell") ){
+            if (etUsername.getText().toString().equals("maxwell") && etPassword.getText().toString().equals("maxwell")) {
 
                 // to move/transit to a new activity follow the below steps
 
@@ -57,8 +61,7 @@ public class Login extends AppCompatActivity {
                 // then complete/finish the starting activity
                 Login.this.finish();
 
-            }
-            else {
+            } else {
                 // show a message as a pop up
                 Toast.makeText(Login.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
             }
@@ -78,15 +81,51 @@ public class Login extends AppCompatActivity {
         tvCall = findViewById(R.id.tvCallExpert);
 
         tvDial.setOnClickListener(view -> {
-            Intent browserIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:3069990000"));
-            Login.this.startActivity(browserIntent);
+            Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:3069990000"));
+            Login.this.startActivity(dialIntent);
         });
 
         tvCall.setOnClickListener(view -> {
-            Intent browserIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:3069990000"));
-            Login.this.startActivity(browserIntent);
+
+            if (ActivityCompat.checkSelfPermission(Login.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(Login.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+            }
+            else {
+                callExpert("tel:3069990000");
+            }
+
         });
 
     }
 
+    private void callExpert(String number) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(number));
+
+        Login.this.startActivity(callIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+
+            case 1: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+                    callExpert("tel:3069990000");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }
